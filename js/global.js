6ea8c244f8a503867cdd11d035908fcba2961e1d
bananaislike <ndomusicchannel@gmail.com>
2023-08-25 11:36:04 +0900

    test

---
+++ js/global.js
@@ -0,0 +1,48 @@
+var logIn = document.querySelector('#log-in');
+var privacy = document.querySelector('#privacy');
+var term = document.querySelector('#term');
+var globalMainLogin = document.querySelector('.global-main--login');
+var dark = document.querySelector('.header-dark');
+var loginBtn = document.querySelector('.login-btn');
+var globalLoginComp =document.querySelector('.global-login--comp');
+var compBtn = document.querySelector('.comp-btn');
+var update = document.querySelector('.update');
+var footerItem = document.querySelectorAll('.footer-item')
+
+
+logIn.addEventListener('click', () =>{
+    globalMainLogin.classList.add('login-active');
+    dark.style.cssText='opacity: 1; z-index: 500';
+})
+
+loginBtn.addEventListener('click', () =>{
+    globalMainLogin.classList.remove('login-active');
+    globalLoginComp.classList.add('login-active');
+})
+
+compBtn.addEventListener('click', () =>{
+    globalLoginComp.classList.remove('login-active');
+    dark.style.cssText='opacity: 0; z-index: -100';
+})
+
+dark.addEventListener('click', () =>{
+    globalMainLogin.classList.remove('login-active');
+    globalLoginComp.classList.remove('login-active');
+    update.classList.remove('login-active');
+    dark.style.cssText='opacity: 0; z-index: -100';
+})
+
+privacy.addEventListener('click', () =>{
+    update.classList.add('login-active');
+    dark.style.cssText='opacity: 1; z-index: 500';
+})
+term.addEventListener('click', () =>{
+    update.classList.add('login-active');
+    dark.style.cssText='opacity: 1; z-index: 500';
+})
+
+
+
+
+// var dark = document.querySelector('#screen-dark');
+// var login = document.querySelector('')
